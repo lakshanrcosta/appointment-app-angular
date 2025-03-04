640751ad5603f9d8bd5a6e63aa615aca3691b811
Lakshan Costa <lakshanrcosta@gmail.com>
2025-03-05 01:46:33 +0200

    Added EsLint and Prettier Configuration

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+// @ts-check
+const js = require("@eslint/js");
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const angular = require("@angular-eslint/eslint-plugin");
+const angularTemplate = require("@angular-eslint/eslint-plugin-template");
+const prettierPlugin = require("eslint-plugin-prettier");
+
+module.exports = [
+    // TypeScript & Angular Configuration
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                sourceType: "module"
+            }
+        },
+        plugins: {
+            "@angular-eslint": angular,
+            "@typescript-eslint": tseslint,
+            prettier: prettierPlugin
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            ...tseslint.configs.stylistic.rules,
+            ...angular.configs.recommended.rules,
+
+            "prettier/prettier": "error",
+            "no-undef": "off",
+
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: "app",
+                    style: "camelCase"
+                }
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: "app",
+                    style: "kebab-case"
+                }
+            ]
+        }
+    },
+
+    // Angular HTML Templates Configuration
+    {
+        files: ["**/*.html"],
+        languageOptions: {
+            parser: require("@angular-eslint/template-parser")
+        },
+        plugins: {
+            "@angular-eslint/template": angularTemplate
+        },
+        rules: {
+            ...angularTemplate.configs.recommended.rules
+        }
+    }
+];
